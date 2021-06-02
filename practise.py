2cb7e3c2034de6cfb5415222734d264eeef2d2e4
alickzheng <83110743+alickzheng@users.noreply.github.com>
2021-06-02 18:12:33 +1000

    prims algo

--- practise.py
+++ practise.py
@@ -1,3 +1,4 @@
+#backtracking algorithms
 def atk(r1, c1, r2, c2):
     return r1 == r2 or c1 == c2 or abs(c1 - c2) == abs(r1 - r2)
 
@@ -35,3 +36,41 @@ def nqueen(n, p = []):
             res += nqueen(n, p + [o])
         return res
 
+#recursions
+def rgcd(x1, x2):
+    if x2 == 0:
+        return x1
+    else:
+        return rgcd(x2, x1%x2)
+
+#sorting algorithms
+import random
+test = random.sample(range(0, 100), 10)
+def rSelSort(lst):
+    res = []
+    if len(lst) <= 1:
+        return lst
+    else:
+        index = lst.index(min(lst))
+        lst[0], lst[index] = lst[index], lst[0]
+        res.append(lst[0])
+        return res + rSelSort(lst[1:])
+
+def insertSort(lst):
+    n = len(lst)
+    if len(lst) <= 1:
+        return lst
+    else:
+        for i in range(n):
+            if i > 1:
+                for j in range(len(lst[:i])):
+                    if lst[i] < lst[j]:
+                        lst[i], lst[j] = lst[j], lst[i]
+        return lst
+
+#spanning tress
+def empty_graph(n):
+    res = [[0 for _ in range(n)] for _ in range(n)]
+    return res
+
+print(empty_graph(3))
